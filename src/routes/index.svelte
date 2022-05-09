055d0b6c38965aa72ec7b2b53c43fd99666f7dac
Ozgur Dincer <ozgurdincer@kentajans.com>
2022-05-10 01:00:10 +0300

    ready steady go

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class=" max-w-7xl mx-auto">
+    <h1 class="text-3xl text-center my-16">
+        Edilli Deniz Kamp ve Karavan
+    </h1>
+
+    <p class="my-9 text-center text-lg">Karadeniz incisi Akçakoca'nın eşsiz konumu Edilli'de doğanın tadını çıkarın. Şehirden uzak keyifli bir zaman. #bungalow #çadır #kamp #karavan #kumsal</p>
+
+    <div class="flex justify-center px-3">
+    <img src="edilli-deniz-kamp-karavan-7.jpeg" alt="Edilli Deniz Kamp & Karavan" class="">
+    </div>
+
+    <div class="flex flex-wrap p-2">
+        <div class="w-1/2 p-1">
+            <img src="edilli-deniz-kamp-karavan-1.jpeg" alt="Edilli Deniz Kamp & Karavan" class="">
+        </div>
+        <div class="w-1/2 p-1">
+            <img src="edilli-deniz-kamp-karavan-2.jpeg" alt="Edilli Deniz Kamp & Karavan" class="">
+        </div>
+        <div class="w-1/2 p-1">    
+            <img src="edilli-deniz-kamp-karavan-3.jpeg" alt="Edilli Deniz Kamp & Karavan" class="">
+        </div>
+        <div class="w-1/2 p-1">    
+            <img src="edilli-deniz-kamp-karavan-4.jpeg" alt="Edilli Deniz Kamp & Karavan" class="">
+        </div>
+        <div class="w-1/2 p-1">    
+            <img src="edilli-deniz-kamp-karavan-5.jpeg" alt="Edilli Deniz Kamp & Karavan" class="">
+        </div>
+        <div class="w-1/2 p-1">    
+            <img src="edilli-deniz-kamp-karavan-6.jpeg" alt="Edilli Deniz Kamp & Karavan" class="">
+        </div>
+    </div>
+
+    <h2 class="text-center text-2xl my-9">Yol Tarifi</h2>
+    <div class="text-center">
+    <a href="http://">Google Harita</a> | 
+        <a href="http://">Yandex Harita</a>  
+    </div>
+
+    <h2 class="text-center text-2xl my-9">İletişim Bilgilerimiz</h2>
+
+    <p class="text-center">Raşit Başar</p>
+    <p  class="text-center">
+        <a href="tel:+905320636676">0532 063 66 76</a>
+    </p>
+    <p  class="text-center">
+        <a href="mailto:info@edillidenizkamp.com">info@edillidenizcamp.com</a>
+    </p>
+
+    <p class="text-center my-9">Aşağıdaki formu da kullanarak bize ulaşabilirsiniz.</p>
+
+
+
+    <div class="mx-auto w-1/2">
+        <iframe title="yadnex form" src="https://forms.yandex.ru/cloud/627982f46921fecc10d57bbd/?iframe=1" frameborder="0" name="ya-form-627982f46921fecc10d57bbd" width="650"></iframe>
+    </div>
+ 
+</main>
+
+<svelte:head>
+    <script src="https://yastatic.net/s3/frontend/forms/_/embed.js"></script>
+</svelte:head>
